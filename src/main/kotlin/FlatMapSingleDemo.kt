c5b96edc79ea418aa31ffe236fc958c730ab6226
xinneph <xinneph@gmail.com>
2020-09-13 20:16:53 +0200

    Minimal version showing issue.

--- src/main/kotlin/FlatMapSingleDemo.kt
+++ src/main/kotlin/FlatMapSingleDemo.kt
@@ -1,34 +1,28 @@
-import com.google.gson.Gson
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 
+private val sampleJson = "\"isMessagingEnabled\": true}"
+private val overrideConfiguration = BehaviorSubject.create<Boolean>().apply { onNext(true) }
 
 fun main() {
-    val localConfigurationFetcher = LocalConfigurationFetcher()
-    val remoteConfigurationFetcher = RemoteConfigurationFetcher()
-    val configurationFetcher: ConfigurationFetcher = DefaultConfigurationFetcher(localConfigurationFetcher, remoteConfigurationFetcher)
-    val configurationLoader = ConfigurationLoader(remoteConfigurationFetcher)
+    val configurationFetcher = ConfigurationFetcher()
+    val configurationLoader = ConfigurationLoader(configurationFetcher)
     configurationLoader.configuration.subscribe()
 }
 
 class ConfigurationLoader(private val configurationFetcher: ConfigurationFetcher) {
-    private val overrideConfiguration = BehaviorSubject.create<Boolean>().apply { onNext(true) }
-    val configuration: Observable<ConfigurationData> = overrideConfiguration
-        .distinctUntilChanged()
-        .switchMap { configurationFetcher.configurationData }
-
-//    val configuration: Observable<ConfigurationData> = configurationFetcher.configurationData
+    val configuration: Observable<String> = overrideConfiguration
+        .switchMap { override ->
+            if (override) {
+                Observable.just(sampleJson)
+            } else {
+                configurationFetcher.configurationData
+            }
+        }
 }
 
-interface ConfigurationFetcher {
-    val configurationData: Observable<ConfigurationData>
-}
-
-data class ConfigurationData(val isMessagingEnabled: Boolean)
-
-class RemoteConfigurationFetcher : ConfigurationFetcher {
-    override val configurationData: Observable<ConfigurationData> = Observable.fromPublisher<String> { subscriber ->
+class ConfigurationFetcher {
+    val configurationData: Observable<String> = Observable.fromPublisher { subscriber ->
         val listener = object : RemoteCallback {
             override fun onSuccess(json: String) {
                 subscriber.onNext(json)
@@ -36,7 +30,7 @@ class RemoteConfigurationFetcher : ConfigurationFetcher {
             }
         }
         RequestCallable().request(listener)
-    }.flatMapSingle { ConfigurationParser().parse(it) }
+    }
 }
 
 interface RemoteCallback {
@@ -45,24 +39,6 @@ interface RemoteCallback {
 
 class RequestCallable {
     fun request(callback: RemoteCallback) {
-        callback.onSuccess("{\"isMessagingEnabled\": true}")
+        callback.onSuccess(sampleJson)
     }
-}
-
-class ConfigurationParser {
-    fun parse(json: String): Single<ConfigurationData> = Single.fromCallable {
-        Gson().fromJson(json, ConfigurationData::class.java)
-    }
-}
-
-class LocalConfigurationFetcher : ConfigurationFetcher {
-    override val configurationData: Observable<ConfigurationData> = Observable.fromCallable {
-//        ConfigurationData(true)
-        throw Throwable("Local configuration is empty")
-    }
-}
-
-class DefaultConfigurationFetcher(local: LocalConfigurationFetcher, remote: RemoteConfigurationFetcher) : ConfigurationFetcher {
-    override val configurationData: Observable<ConfigurationData> = local.configurationData
-        .onErrorResumeWith(remote.configurationData)
 }
